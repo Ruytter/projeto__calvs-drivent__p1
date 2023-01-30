8293f468c1d17c8fbbaf79a25d994445dc7946ac
Ruytter <ruytter.benjamin@gmail.com>
2023-01-30 15:41:08 -0300

    fix: tickets/types route

--- src/routers/tickets-router.ts
+++ src/routers/tickets-router.ts
@@ -1,12 +1,17 @@
 import { Router } from "express";
-import { authenticateToken } from "@/middlewares";
+import { authenticateToken, validateBody } from "@/middlewares";
 import { getAllTypesOfTickets } from "@/controllers";
+// import { createEnrollmentSchema } from "@/schemas";
 
 const ticketsRouter = Router();
 
 ticketsRouter
 
   .all("/*", authenticateToken)
-  .get("/tickets/types", getAllTypesOfTickets); //Retorna todos os tipos de ticket
-  
+  .get("/tickets/types", getAllTypesOfTickets) //Retorna todos os tipos de ticket
+  // .get("/tickets", getEnrollmentByUser) //Retorna o ticket do usuário
+  // .post("/tickets", validateBody(createEnrollmentSchema), postCreateOrUpdateEnrollment) //Cria um novo ticket para o usuário baseado no tipo de ticket
+  // .get("/payments?ticketId=1", getEnrollmentByUser) //Retorna a informação de pagamento do ticket do usuário
+  // .post("/tickets", validateBody(createEnrollmentSchema), postCreateOrUpdateEnrollment); //Cria um novo pagamento para o ticket
+
 export { ticketsRouter };
