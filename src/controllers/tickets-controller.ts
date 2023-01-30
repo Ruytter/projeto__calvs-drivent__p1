041ba8a22ee8ff87d33db669a6c6fd7254ebee7c
Ruytter <ruytter.benjamin@gmail.com>
2023-01-30 16:53:38 -0300

    feat: tickets-controller

--- src/controllers/tickets-controller.ts
+++ src/controllers/tickets-controller.ts
@@ -13,3 +13,49 @@ export async function getAllTypesOfTickets(req: AuthenticatedRequest, res: Respo
     }
   }
 }
+
+export async function getTicketByUser(req: AuthenticatedRequest, res: Response) {
+  try {
+    const ticket = await ticketsService.getTicket();
+    return res.status(httpStatus.OK).send(ticket);
+  } catch (error) {
+    if (error.name === "NotFoundError") {
+      return res.send(httpStatus.NO_CONTENT);
+    }
+  }
+}
+
+export async function postCreateTicket(req: AuthenticatedRequest, res: Response) {
+  try {
+    await ticketsService.CreateTicket({
+      ...req.body,
+      userId: req.userId,
+    });
+    return res.sendStatus(httpStatus.OK);
+  } catch (error) {
+    return res.sendStatus(httpStatus.BAD_REQUEST);
+  }
+}
+
+export async function postCreateTicketPayment(req: AuthenticatedRequest, res: Response) {
+  try {
+    await ticketsService.createPayment({
+      ...req.body,
+      userId: req.userId,
+    });
+    return res.sendStatus(httpStatus.OK);
+  } catch (error) {
+    return res.sendStatus(httpStatus.BAD_REQUEST);
+  }
+}
+
+export async function getPaymentByTicketId(req: AuthenticatedRequest, res: Response) {
+  try {
+    const ticket = await ticketsService.getPayment();
+    return res.status(httpStatus.OK).send(ticket);
+  } catch (error) {
+    if (error.name === "NotFoundError") {
+      return res.send(httpStatus.NO_CONTENT);
+    }
+  }
+}  
--- src/routers/tickets-router.ts
+++ src/routers/tickets-router.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { authenticateToken, validateBody } from "@/middlewares";
-import { getAllTypesOfTickets, getTicketByUser, CreateTicket, getPaymentsByTicketId, CreateTicketPayment } from "@/controllers";
+import { getAllTypesOfTickets, getTicketByUser, postCreateTicket, getPaymentByTicketId, postCreateTicketPayment } from "@/controllers";
 import { createTicketchema, createPaymenteSchema } from "@/schemas";
 
 const ticketsRouter = Router();
@@ -10,8 +10,8 @@ ticketsRouter
   .all("/*", authenticateToken)
   .get("/tickets/types", getAllTypesOfTickets) //Retorna todos os tipos de ticket
   .get("/tickets", getTicketByUser) //Retorna o ticket do usuário
-  .post("/tickets", validateBody(createTicketchema), CreateTicket) //Cria um novo ticket para o usuário baseado no tipo de ticket
-  .get("/payments?ticketId=1", getPaymentsByTicketId) //Retorna a informação de pagamento do ticket do usuário
-  .post("/payments/process", validateBody(createPaymenteSchema), CreateTicketPayment); //Cria um novo pagamento para o ticket
+  .post("/tickets", validateBody(createTicketchema), postCreateTicket) //Cria um novo ticket para o usuário baseado no tipo de ticket
+  .get("/payments?ticketId=1", getPaymentByTicketId) //Retorna a informação de pagamento do ticket do usuário
+  .post("/payments/process", validateBody(createPaymenteSchema), postCreateTicketPayment); //Cria um novo pagamento para o ticket
 
 export { ticketsRouter };
